44d3382431d82c2a68f8b037b748b4b14f6be30c
AntonioNtV <antoniontv18@gmail.com>
2020-09-04 20:32:10 -0300

    Defining global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import GlobalSytle from './styles/global';
 
-const App: React.FC = () => <h1>Hello World</h1>;
+const App: React.FC = () => (
+  <>
+    <GlobalSytle />
+    <h1>Hello World</h1>
+  </>
+);
 
 export default App;
