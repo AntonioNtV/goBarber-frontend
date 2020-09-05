03ffca261e0cd7ff2c5a933b75ccf2d23d582ae2
AntonioNtV <antoniontv18@gmail.com>
2020-09-05 08:18:28 -0300

    Sign up page

---
+++ src/pages/Signup/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {
+  FiArrowLeft, FiMail, FiLock, FiUser,
+} from 'react-icons/fi';
+import { Container, Content, Background } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import logoImg from '../../assets/logo.svg';
+
+const Signup: React.FC = () => (
+  <>
+    <Container>
+      <Background />
+
+      <Content>
+        <img src={logoImg} alt="goBarber" />
+        <form>
+          <Input name="username" icon={FiUser} placeholder="Nome" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
+          <Button type="submit">Cadastrar</Button>
+        </form>
+
+        <a href="create">
+          <FiArrowLeft />
+          Voltar para o login
+        </a>
+      </Content>
+    </Container>
+  </>
+);
+
+export default Signup;
---
+++ src/pages/Signup/styles.ts
@@ -0,0 +1,68 @@
+import styled from 'styled-components';
+import { shade } from 'polished';
+import signInBackground from '../../assets/sign-in-background.png';
+
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: stretch;
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  place-content: center;
+
+  width: 100%;
+  max-width: 700px;
+
+  form {
+    margin: 80px 0;
+    width: 340px;
+    text-align: center;
+    h1 {
+    margin-bottom: 24px;
+  }
+
+    a {
+      color: #F4EDE8;
+      display: block;
+      text-decoration: none;
+      margin-top: 24px;
+      transition: color 0.2s;
+
+      &:hover {
+        color: ${shade(0.2, '#F4EDE8')}
+      }
+    }
+  }
+
+  > a {
+      display: flex;
+      align-items: center;
+
+      color: #FF9000;
+      text-decoration: none;
+      margin-top: 24px;
+      transition: color 0.2s;
+
+      &:hover {
+        color: ${shade(0.2, '#FF9000')}
+      }
+
+      svg {
+        margin-right: 18.5px;
+      }
+
+  }
+
+`;
+
+const Background = styled.div`
+  flex: 1;
+  background: url(${signInBackground}) no-repeat center;
+  background-size: cover;
+`;
+
+export { Container, Content, Background };
