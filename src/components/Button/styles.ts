4dd1d8e740d33d6cb4a33568378222665eedfd89
AntonioNtV <antoniontv18@gmail.com>
2020-09-05 08:09:06 -0300

    Moving button style from signin page to button component

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,5 +1,21 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
-const Container = styled.button``;
+const Container = styled.button`
+      background: #FF9000;
+      height: 56px;
+      border-radius: 10px;
+      border: 0;
+      padding: 0 16px;
+      width: 100%;
+      color: #312e38;
+      font-weight: 500;
+      margin-top: 16px;
+      transition: background-color 0.2s;
+
+      &:hover {
+        background-color: ${shade(0.2, '#FF9000')}
+
+    }`;
 
 export { Container };
--- src/pages/Signin/styles.ts
+++ src/pages/Signin/styles.ts
@@ -25,24 +25,6 @@ const Content = styled.div`
     margin-bottom: 24px;
   }
 
-    button {
-      background: #FF9000;
-      height: 56px;
-      border-radius: 10px;
-      border: 0;
-      padding: 0 16px;
-      width: 100%;
-      color: #312e38;
-      font-weight: 500;
-      margin-top: 16px;
-      transition: background-color 0.2s;
-
-      &:hover {
-        background-color: ${shade(0.2, '#FF9000')}
-      }
-
-    }
-
     a {
       color: #F4EDE8;
       display: block;
