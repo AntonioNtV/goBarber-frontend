0f0fd4064d9bbff035dd3a70ecd6e79db05f3eff
AntonioNtV <antoniontv18@gmail.com>
2020-09-05 07:48:32 -0300

    Fixing styled component type

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,10 +4,9 @@ import { Container } from './styles';
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
 
 const Button: React.FC<ButtonProps> = ({ children, ...rest }) => (
-  <Container>
-    <button type="button" {...rest}>{children}</button>
+  <Container type="button" {...rest}>
+    {children}
   </Container>
-
 );
 
 export default Button;
