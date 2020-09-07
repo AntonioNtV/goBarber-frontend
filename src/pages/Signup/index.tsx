a2ca460aec98c538baad0eb12593b4d1acc1622c
AntonioNtV <antoniontv18@gmail.com>
2020-09-07 18:03:59 -0300

    Animating sign up

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -8,7 +8,9 @@ import { Link } from 'react-router-dom';
 
 import * as Yup from 'yup';
 
-import { Container, Content, Background } from './styles';
+import {
+  Container, Content, Background, AnimationContainer,
+} from './styles';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import logoImg from '../../assets/logo.svg';
@@ -46,19 +48,21 @@ const Signup: React.FC = () => {
       <Background />
 
       <Content>
-        <img src={logoImg} alt="goBarber" />
-        <Form ref={formRef} onSubmit={handleSubmit}>
-          <h1>Faça seu cadastro</h1>
-          <Input name="name" icon={FiUser} placeholder="Nome" />
-          <Input name="email" icon={FiMail} placeholder="E-mail" />
-          <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
-          <Button type="submit">Cadastrar</Button>
-        </Form>
+        <AnimationContainer>
+          <img src={logoImg} alt="goBarber" />
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Faça seu cadastro</h1>
+            <Input name="name" icon={FiUser} placeholder="Nome" />
+            <Input name="email" icon={FiMail} placeholder="E-mail" />
+            <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
+            <Button type="submit">Cadastrar</Button>
+          </Form>
 
-        <Link to="/">
-          <FiArrowLeft />
-          Voltar para o login
-        </Link>
+          <Link to="/">
+            <FiArrowLeft />
+            Voltar para o login
+          </Link>
+        </AnimationContainer>
       </Content>
     </Container>
   );
--- src/pages/Signup/styles.ts
+++ src/pages/Signup/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { shade } from 'polished';
 import signUpBackground from '../../assets/sign-up-background.png';
 
@@ -8,6 +8,66 @@ const Container = styled.div`
   align-items: stretch;
 `;
 
+const appearFromRight = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(50px)
+  }
+
+  to {
+    opacity: 1;
+    transform: translateX(0px)
+  }
+
+`;
+
+const AnimationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  animation: ${appearFromRight} 1s;
+
+  form {
+    margin: 80px 0;
+    width: 340px;
+    text-align: center;
+    h1 {
+    margin-bottom: 24px;
+  }
+
+    a {
+      color: #F4EDE8;
+      display: block;
+      text-decoration: none;
+      margin-top: 24px;
+      transition: color 0.2s;
+
+      &:hover {
+        color: ${shade(0.2, '#F4EDE8')}
+      }
+    }
+  }
+
+  > a {
+      display: flex;
+      align-items: center;
+
+      color: #FF9000;
+      text-decoration: none;
+      margin-top: 24px;
+      transition: color 0.2s;
+
+      &:hover {
+        color: ${shade(0.2, '#FF9000')}
+      }
+
+      svg {
+        margin-right: 18.5px;
+      }
+
+  }`;
+
 const Content = styled.div`
   display: flex;
   flex-direction: column;
@@ -16,36 +76,6 @@ const Content = styled.div`
 
   width: 100%;
   max-width: 700px;
-
-  form {
-    margin: 80px 0;
-    width: 340px;
-    text-align: center;
-
-    h1 {
-      margin-bottom: 24px;
-    }
-  }
-
-  > a {
-      display: flex;
-      align-items: center;
-
-      color: #F4EDE8;
-      text-decoration: none;
-      margin-top: 24px;
-      transition: color 0.2s;
-
-      &:hover {
-        color: ${shade(0.2, '#F4EDE8')}
-      }
-
-      svg {
-        margin-right: 18.5px;
-      }
-
-  }
-
 `;
 
 const Background = styled.div`
@@ -54,4 +84,6 @@ const Background = styled.div`
   background-size: cover;
 `;
 
-export { Container, Content, Background };
+export {
+  Container, Content, Background, AnimationContainer,
+};
