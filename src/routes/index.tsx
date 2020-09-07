431625bcd2db3f9d72c3d93252553d761c4a5c41
AntonioNtV <antoniontv18@gmail.com>
2020-09-07 19:02:26 -0300

    Auth route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import Signin from '../pages/Signin/index';
 import Signup from '../pages/Signup/index';
+import Dashboard from '../pages/Dashboard/index';
+import Route from './Route';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route exact path="/" component={Signin} />
     <Route exact path="/signup" component={Signup} />
+    <Route exact path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
 );
 
