3fe03584d0f0c9323e3ba23b1134e4b25c7a0287
AntonioNtV <antoniontv18@gmail.com>
2020-09-07 12:51:43 -0300

    Styling toast

--- src/components/ToastContainer/styles.ts
+++ src/components/ToastContainer/styles.ts
@@ -1,5 +1,83 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-const Container = styled.div``;
-const Toast = styled.div``;
+interface ToastProps {
+  type?: 'success' | 'error' | 'info';
+  hasDescription: boolean;
+}
+
+const Container = styled.div`
+  position: absolute;
+  right: 0;
+  top: 0;
+  padding: 30px;
+  overflow: hidden;
+`;
+
+const toastTypeVariations = {
+  success: css`
+    background-color: #E6FFFA;
+    color: #2E656A;
+  `,
+  info: css`
+    background-color: #EBF8FF;
+    color: #3172B7;
+  `,
+  error: css`
+  background-color: #FDDEDE;
+    color: #C53030;
+  `,
+};
+
+const Toast = styled.div<ToastProps>`
+  width: 360px;
+
+  position: relative;
+  padding: 16px 30px 16px 16px;
+  border-radius: 10px;
+  box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.2);
+
+  background-color: #ebf8ff;
+  color: #3172b7;
+
+  & + div {
+    margin-top: 8px;
+  }
+
+
+  ${(props) => toastTypeVariations[props.type || 'info']}
+  display: flex;
+
+  > svg {
+    margin: 4px 12px 0 0;
+  }
+
+  div {
+    flex: 1;
+
+    p {
+      margin-top: 4px;
+      font-size: 14px;
+      opacity: 0.8;
+      line-height: 20px;
+    }
+  }
+
+  button {
+    position: absolute;
+    right: 16px;
+    top: 19px;
+    opacity: 0.8;
+    border: 0;
+    background: transparent;
+    color: inherit;
+  }
+
+  ${(props) => !props.hasDescription && css`
+    align-items: center;
+
+    svg {
+      margin-top: 0;
+    }
+  `}
+`;
 export { Container, Toast };
