ea1d6bcc2b91fa9bf29b834c5d7a0d04bcf20fb9
AntonioNtV <antoniontv18@gmail.com>
2020-09-07 09:32:57 -0300

    Validaçao de login

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,23 +1,47 @@
-import React from 'react';
+import React, { useRef, useCallback } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
 import { Container, Content, Background } from './styles';
 import logoImg from '../../assets/logo.svg';
 import Button from '../../components/Button/index';
 import Input from '../../components/Input/index';
+import getValidationErrors from '../../utils/getValidationErros';
 
-const Signin: React.FC = () => (
-  <>
+const Signin: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data: Request) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        email: Yup.string().required('Email obrigatório').email('Digite um e-mail válido'),
+        password: Yup.string().required('Senha obrigatória'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (error) {
+      const errors = getValidationErrors(error);
+        formRef.current?.setErrors(errors);
+    }
+  }, []);
+
+  return (
     <Container>
       <Content>
         <img src={logoImg} alt="goBarber" />
-        <form>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu logon</h1>
           <Input name="email" icon={FiMail} placeholder="E-mail" />
           <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
           <Button type="submit">Entrar</Button>
           <a href="forgot">Esqueci minha senha</a>
-        </form>
+        </Form>
 
         <a href="create">
           <FiLogIn />
@@ -26,7 +50,7 @@ const Signin: React.FC = () => (
       </Content>
       <Background />
     </Container>
-  </>
-);
+  );
+};
 
 export default Signin;
