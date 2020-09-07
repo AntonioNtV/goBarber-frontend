971e11ef0461bbb5eadeed2910a98659e1be8b07
AntonioNtV <antoniontv18@gmail.com>
2020-09-07 18:00:16 -0300

    Sign in animation

--- src/pages/Signin/styles.ts
+++ src/pages/Signin/styles.ts
@@ -1,11 +1,26 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { shade } from 'polished';
 import signInBackground from '../../assets/sign-in-background.png';
+
+const appearFromLeft = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(-50px)
+  }
+
+  to {
+    opacity: 1;
+    transform: translateX(0px)
+  }
+
+`;
 
 const AnimationContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  animation: ${appearFromLeft} 1s;
 
   form {
     margin: 80px 0;
