35dfee4525d58728a699957dadd7ef82c5ab1efc
AntonioNtV <antoniontv18@gmail.com>
2020-09-07 16:10:43 -0300

    Using app provider

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,15 @@ import GlobalSytle from './styles/global';
 import Signin from './pages/Signin/index';
 import Signup from './pages/Signup/index';
 
-import { AuthProvider } from './hooks/auth';
-import { ToastProvider } from './hooks/toast';
+import AppProvider from './hooks';
 import { ToastContainer } from './components/ToastContainer';
 
 const App: React.FC = () => (
   <>
     <GlobalSytle />
-    <AuthProvider>
+    <AppProvider>
       <Signin />
-    </AuthProvider>
+    </AppProvider>
     <ToastContainer />
   </>
 );
